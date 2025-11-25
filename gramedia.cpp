7c38bc3f181a593f73215c7e9ca0743d8173f84a
bozu1304 <tanteibozu@gmail.com>
2025-11-25 14:40:33 +0700

    menambahkan createNode

--- gramedia.cpp
+++ gramedia.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
+using namespace std;
 
-struct Node
+struct Node{
+    string judulbuku;
+    int ID;
+    Node* left;
+    Node* right;
+}
+
+Node *createNode(int ID, string judulbuku)
 {
-    int id;
-    std::string judul;
+    Node* newNode = new Node();
+    newNode->ID = ID;
+    newNode->left = newNode->right = nullptr;
+    return newNode;
+}
 
-    Node *left;
-    Node *right;
-};
-
+Node *insertNode(Node* root, int ID, string judulbuku)
+{
+    if (root == nullptr){
+        return createNode(ID, judulbuku);
+    }
+    if (ID < root->ID){
+        root->left = insertNode(root->left, ID, judulbuku);
+    }
+    else if (ID > root->ID){
+        root->right = insertNode(root->right, ID, judulbuku);
+    }
+    return root;
+}
